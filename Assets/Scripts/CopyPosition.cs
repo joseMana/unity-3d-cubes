c8251d3bb98d5e00cd12b48f180371dac6209669
joseMana <josemanangan@gmail.com>
2021-04-26 01:53:32 +0800

    - created copy position class for dynamic camera view  - change name in unity "Sphere to Player"

---
+++ Assets/Scripts/CopyPosition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CopyPosition : MonoBehaviour
+{
+    [SerializeField]
+    Transform transformTarget;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transformTarget.position;
+    }
+}
