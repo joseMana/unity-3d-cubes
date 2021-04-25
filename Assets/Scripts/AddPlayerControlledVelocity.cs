3ea052f3b53905cb8d0bd9a62c99a6bffdf748fa
joseMana <josemanangan@gmail.com>
2021-04-26 01:19:03 +0800

    - add AddPlayerControlledVelocity class

---
+++ Assets/Scripts/AddPlayerControlledVelocity.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddPlayerControlledVelocity : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 v3_forceVelocity;
+
+    [SerializeField]
+    KeyCode keyPositive;
+
+    [SerializeField]
+    KeyCode keyNegative;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKey(keyPositive))
+            GetComponent<Rigidbody>().velocity += v3_forceVelocity;
+
+        if (Input.GetKey(keyNegative))
+            GetComponent<Rigidbody>().velocity -= v3_forceVelocity;
+
+    }
+}
